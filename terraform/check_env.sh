ee9b79cb65e36d532f2f8e5bbae1237d5ac43106
ikemonn <daiki.matsui@plaid.co.jp>
2015-12-16 21:10:28 +0900

    commig

---
+++ terraform/check_env.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# これを参考にスクリプトを書いた →http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming
+
+# 引数で渡されたディレクトリのファイルが変更されたか？
+is_file_changed() {
+  local environment=$1
+  # 実行ステータスだけ取得したいので、grepの結果は出力しない
+  git diff --name-only HEAD HEAD^ \
+    | grep $environment/ >> /dev/null
+  echo $?
+}
+
+# ファイルが変更されたディレクトリの配列を取得
+get_file_change_environment_list() {
+  local environment
+  for environment in ${ALL_ENVIRONMENT[@]}
+  do
+    if [ `is_file_changed $environment` == "0" ]; then
+      FILE_CHANGE_ENVIRONMENT+=($environment)
+    fi
+  done
+  echo ${FILE_CHANGE_ENVIRONMENT[@]}
+}
+
+# terraform planを与えられた環境で行う
+exec_terraform_plan_per_environment() {
+  local environment_list=$@
+  for environment in ${environment_list[@]}
+  do
+    cd $TERRAFORM_DIR/$environment
+    echo `pwd`
+    cd -
+  done
+}
+
+main() {
+  readonly ALL_ENVIRONMENT=("evaluation" "staging" "production")
+  readonly EVALUATION="evaluation"
+  readonly STAGING="staging"
+  readonly PRODUCTION="production"
+  readonly TERRAFORM_DIR="terraform/aws/"
+
+  FILE_CHANGE_ENVIRONMENT=()
+
+  # ファイルが変更された環境を取得
+  file_change_environment=`get_file_change_environment_list`
+  # ファイルが変更された環境でterraform planを実行
+  exec_terraform_plan_per_environment ${file_change_environment[@]}
+
+
+}
+
+main
